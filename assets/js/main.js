fa8acff6200b53cee3b77a8906468c1097173abe
Sujeevan <sujeevanbalamany@gmail.com>
2024-07-19 16:09:44 +0530

    feat: display current day image

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-// Write your javascript code here
+const dateFormEl = document.querySelector("#dateForm")
+const dayImageContainerEl = document.querySelector("#dayImage")
+
+const myDate = new Date(); // Assuming myDate is a Date object
+
+const year = myDate.getFullYear();
+const month = String(myDate.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+const day = String(myDate.getDate()).padStart(2, '0');
+
+const result = `${year}-${month}-${day}`;
+
+const finalResult = `https://www.prokerala.com/general/calendar/imgs/daily/${result}.png`
+
+dayImageContainerEl.src = finalResult;
+dayImageContainerEl.alt = result;
+
+console.log(finalResult);
