e0c90230fcb89bb0e1c54fa1312128ac0fe0fcea
Sujeevan <sujeevanbalamany@gmail.com>
2024-07-19 16:46:27 +0530

    get date image based on inpt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,33 @@
 const dateFormEl = document.querySelector("#dateForm")
 const dayImageContainerEl = document.querySelector("#dayImage")
 
-const myDate = new Date(); // Assuming myDate is a Date object
+dateFormEl.addEventListener("submit",(e)=>{
+    e.preventDefault();
 
-const year = myDate.getFullYear();
-const month = String(myDate.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
-const day = String(myDate.getDate()).padStart(2, '0');
+    const inputDate = e.target[0].value;
 
-const result = `${year}-${month}-${day}`;
+    processDate(inputDate);
+})
 
-const finalResult = `https://www.prokerala.com/general/calendar/imgs/daily/${result}.png`
 
-dayImageContainerEl.src = finalResult;
-dayImageContainerEl.alt = result;
 
-console.log(finalResult);
+function processDate(date){
+
+    const myDate = date ?  new Date(date) :  new Date()
+
+    const year = myDate.getFullYear();
+    const month = String(myDate.getMonth() + 1).padStart(2, '0'); 
+    const day = String(myDate.getDate()).padStart(2, '0');
+    
+    const result = `${year}-${month}-${day}`;
+    
+    const finalResult = `https://www.prokerala.com/general/calendar/imgs/daily/${result}.png`
+    
+    dayImageContainerEl.src = finalResult;
+    dayImageContainerEl.alt = result;
+    
+    console.log(finalResult);
+};
+
+processDate();
+
